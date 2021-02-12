3465ad35db089a36be3e8aa7216a3c97d94e8aa9
rachwongrw <rwrachelwong@gmail.com>
2021-02-12 10:58:31 -0800

    add second screen tab in bottom navigation

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
+import BottomTabNavigator from './src/components/navigation/BottomTabNavigator'
 import ScreenOne from './src/screens/ScreenOne'
 import ScreenTwoA from './src/screens/ScreenTwoA'
 import ScreenTwoB from './src/screens/ScreenTwoB'
@@ -13,9 +14,9 @@ export default function App() {
     <SafeAreaProvider>
       <NavigationContainer>
         <Stack.Navigator screenOptions={{headerShown: false}}>
-          <Stack.Screen name="Root" component={ScreenOne}/>
+          <Stack.Screen name="Root" component={BottomTabNavigator}/>
           <Stack.Screen name="ScreenTwoA" component={ScreenTwoA}/>
-          <Stack.Screen name="ScreenTwoB" component={ScreenTwoB}/>
+          <Stack.Screen name="ScreenTwoB" component={ScreenTwoB}/> 
         </Stack.Navigator>
       </NavigationContainer>
     </SafeAreaProvider>
--- src/components/navigation/BottomTabNavigator.js
+++ src/components/navigation/BottomTabNavigator.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
-import { createStackNavigator } from '@react-navigation/stack';
-import ScreenOne from '../../screens/ScreenOne';
+import { createStackNavigator } from '@react-navigation/stack'
+import ScreenOne from '../../screens/ScreenOne'
+import { EvilIcons } from '@expo/vector-icons'
+import ScreenTwoA from '../../screens/ScreenTwoA'
 
 const BottomTab = createBottomTabNavigator()
 const ScreenOneStack = createStackNavigator()
+const ScreenTwoStack = createStackNavigator()
 
 export default function BottomTabNavigator() {
 
@@ -14,9 +17,19 @@ export default function BottomTabNavigator() {
       <BottomTab.Screen 
         name="ScreenOne"
         component={ScreenOneNavigator}
+        options={{
+          tabBarIcon: () => <EvilIcons name="star" size={30} color="black"/>
+        }}
       />
       {/* ScreenTwo A/B Stack */}
-      {/* <BottomTab.Screen /> */}
+      <BottomTab.Screen
+        name="ScreenTwo"
+        component={ScreenTwoNavigator}
+        options={{
+          tabBarIcon: () => <EvilIcons name="user" size={30} color="black"/>
+        }}
+      />
+      
     </BottomTab.Navigator>
   );
 }
@@ -30,5 +43,16 @@ const ScreenOneNavigator = () => {
         options={{headerTitle: 'Single-Level Stack'}}
       />
     </ScreenOneStack.Navigator>
+  )
+}
+
+const ScreenTwoNavigator = () => {
+  return (
+    <ScreenTwoStack.Navigator>
+      <ScreenTwoStack.Screen
+        name="ScreenTwo"
+        component={ScreenTwoA}
+      />
+    </ScreenTwoStack.Navigator>
   )
 }
